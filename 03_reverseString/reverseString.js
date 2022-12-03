d8148f24429ac5c6f8412b324028e5656b5dd69a
Shannon chaulet <shannonchaulet@Shannons-MacBook-Pro.local>
2022-12-02 21:00:06 -0500

    Reorganized folder structure

--- 03_reverseString/reverseString.js
+++
@@ -1,9 +0,0 @@
-const reverseString = function(str) {
-    let split = str.split("");
-    let reverse = split.reverse();
-    let join = reverse.join("");
-    return join;
-};
-
-// Do not edit below this line
-module.exports = reverseString;
